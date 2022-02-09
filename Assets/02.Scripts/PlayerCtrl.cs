d7ea96b06bb5429747bbba8bb6a04f1fb30167d5
Lee Jae Hyun <myleje@gmail.com>
2022-02-09 11:12:38 +0900

    .

--- Assets/02.Scripts/PlayerCtrl.cs
+++ Assets/02.Scripts/PlayerCtrl.cs
@@ -1,18 +1,29 @@
 using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
+using UnityEngine.AI;
 
 public class PlayerCtrl : MonoBehaviour
 {
-    // Start is called before the first frame update
+    private NavMeshAgent agent;
+
+    private Camera camera;
+
+    // Ray, RaycastHit
+    private Ray ray;
+    private RaycastHit hit;
+
     void Start()
     {
-        
+        agent = GetComponent<NavMeshAgent>();
+        camera = Camera.main;
     }
 
-    // Update is called once per frame
     void Update()
     {
-        
+        // 스크린의 2차원 좌표(x,y) -> 3D 투영하는 광선을 생성
+        ray = camera.ScreenPointToRay(Input.mousePosition);
+
+        Debug.DrawRay(ray.origin, ray.direction * 100.0f, Color.green);
     }
 }
