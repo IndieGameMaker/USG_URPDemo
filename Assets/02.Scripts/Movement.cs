0e9b35963042bd3d5be5e2fa77dbc440083af7bf
Lee Jae Hyun <myleje@gmail.com>
2022-02-09 15:12:25 +0900

    .

--- Assets/02.Scripts/Movement.cs
+++ Assets/02.Scripts/Movement.cs
@@ -9,6 +9,15 @@ public class Movement : MonoBehaviour
     private new Transform transform;
     private new Camera camera;
 
+    // 가상의 Plane
+    private Plane plane;
+    private Ray ray;
+    private RaycastHit hit;
+
+    // 키보드 입력값
+    float h => Input.GetAxis("Horizontal");
+    float v => Input.GetAxis("Vertical");
+
     // Start is called before the first frame update
     void Start()
     {
@@ -16,10 +25,17 @@ public class Movement : MonoBehaviour
         anim = GetComponent<Animator>();
         transform = GetComponent<Transform>();
         camera = Camera.main;
+
+        plane = new Plane(transform.up, transform.position);
     }
 
     // Update is called once per frame
     void Update()
+    {
+        Move();
+    }
+
+    void Move()
     {
 
     }
